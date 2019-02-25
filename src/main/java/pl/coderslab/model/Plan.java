1f1e1efd30fb3b5e1400a5e2b91e4462a67931b6
milosz <jarmulamilosz@gmail.com>
2019-02-25 23:37:15 +0100

    Plan and PlanDao

---
+++ src/main/java/pl/coderslab/model/Plan.java
@@ -0,0 +1,68 @@
+package pl.coderslab.model;
+
+public class Plan {
+    private  int id;
+    private String name;
+    private String description;
+    private String created; // dataTime
+    private int adminId;
+
+    public Plan(String name, String description, String created) {
+        this.name = name;
+        this.description = description;
+        this.created = created;
+    }
+    public Plan(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    @Override
+    public String toString() {
+        return "Plan{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", created='" + created + '\'' +
+                ", adminId=" + adminId +
+                '}';
+    }
+}

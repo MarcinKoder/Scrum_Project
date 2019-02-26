27090a83b9067e4fdbf5488f88dd73c655d0266d
madziollina <magdziollina@gmail.com>
2019-02-27 00:20:49 +0100

    RecipeDao - count recipes added by admin

--- src/main/java/pl/coderslab/dao/RecipeDao.java
+++ src/main/java/pl/coderslab/dao/RecipeDao.java
@@ -20,7 +20,7 @@ public class RecipeDao {
     private static final String FIND_ALL_RECIPE_QUERY = "SELECT * from recipe";
     private static final String READ_RECIPE_QUERY = "SELECT * from recipe where id = ?";
     private static final String UPDATE_RECIPE_QUERY = "UPDATE recipe SET name = ?, ingredients = ?, description = ?, updated = ?, preparation_time = ?, preparation = ?, admin_id = ? WHERE	id = ?";
-
+    private static final String FIND_ALL_RECIPES_BY_ADMIN_QUERY = "SELECT * from recipe where admin_id = ?";
     /**
      * Get recipe by id
      *
@@ -171,6 +171,34 @@ public class RecipeDao {
             e.printStackTrace();
         }
 
+    }
+
+    public Integer count(Integer adminId){
+        List<Recipe> recipeList = new ArrayList<>();
+
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(FIND_ALL_RECIPES_BY_ADMIN_QUERY);
+        ) {
+            statement.setInt(1, adminId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    Recipe recipe = new Recipe();
+                    recipe.setId(resultSet.getInt("id"));
+                    recipe.setName(resultSet.getString("name"));
+                    recipe.setIngredients(resultSet.getString("ingredients"));
+                    recipe.setDescription(resultSet.getString("description"));
+                    recipe.setCreated(resultSet.getString("created"));
+                    recipe.setUpdated(resultSet.getString("updated"));
+                    recipe.setPreparation_time(resultSet.getInt("preparation_time"));
+                    recipe.setPreparation(resultSet.getString("preparation"));
+                    recipe.setAdmin_id(resultSet.getInt("admin_id"));
+                    recipeList.add(recipe);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recipeList.size();
     }
 
 }

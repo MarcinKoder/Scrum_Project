6e69549e80edac337938f249f921413735514352
madziollina <magdziollina@gmail.com>
2019-02-27 09:16:51 +0100

    RecipeDao - count and updated update and delete

--- src/main/java/pl/coderslab/dao/RecipeDao.java
+++ src/main/java/pl/coderslab/dao/RecipeDao.java
@@ -21,6 +21,7 @@ public class RecipeDao {
     private static final String READ_RECIPE_QUERY = "SELECT * from recipe where id = ?";
     private static final String UPDATE_RECIPE_QUERY = "UPDATE recipe SET name = ?, ingredients = ?, description = ?, updated = ?, preparation_time = ?, preparation = ?, admin_id = ? WHERE	id = ?";
     private static final String FIND_ALL_RECIPES_BY_ADMIN_QUERY = "SELECT * from recipe where admin_id = ?";
+
     /**
      * Get recipe by id
      *
@@ -131,21 +132,20 @@ public class RecipeDao {
      *
      * @param recipeId
      */
-    public void delete(Integer recipeId) {
+    public boolean delete(Integer recipeId) {
+        boolean deleted = false;
         try (Connection connection = DbUtil.getConnection();
              PreparedStatement statement = connection.prepareStatement(DELETE_RECIPE_QUERY);) {
             statement.setInt(1, recipeId);
             int isDeleted = statement.executeUpdate();
 
-            boolean deleted = isDeleted != 0;
-            if (!deleted) {
-                throw new NotFoundException("Product not found");
-            }
+            deleted = isDeleted != 0;
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        return deleted;
     }
 
 
@@ -154,7 +154,8 @@ public class RecipeDao {
      *
      * @param recipe
      */
-    public void update(Recipe recipe) {
+    public boolean update(Recipe recipe) {
+        boolean updated = false;
         try (Connection connection = DbUtil.getConnection();
              PreparedStatement statement = connection.prepareStatement(UPDATE_RECIPE_QUERY);) {
             statement.setInt(8, recipe.getId());
@@ -166,14 +167,17 @@ public class RecipeDao {
             statement.setString(6, recipe.getPreparation());
             statement.setString(7, String.valueOf(recipe.getAdmin_id()));
 
-            statement.executeUpdate();
+            int isUpdated = statement.executeUpdate();
+
+            updated = isUpdated != 0;
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return updated;
 
     }
 
-    public Integer count(Integer adminId){
+    public Integer count(Integer adminId) {
         List<Recipe> recipeList = new ArrayList<>();
 
         try (Connection connection = DbUtil.getConnection();
